939fbf2223a2c2db48072dacb59fe3512c3a6531
josh <josh.timmons@hashicorp.com>
2023-06-18 18:59:23 -0400

    Add git add to pre-commit
    Signed-off-by: josh <josh.timmons@hashicorp.com>

--- tf/dynamodb.tf
+++ tf/dynamodb.tf
@@ -2,8 +2,8 @@ module "dynamodb-table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
   version = "3.3.0"
 
-  name     = "echo-table"
-  hash_key = "id"
+  name                        = "echo-table"
+  hash_key                    = "id"
   deletion_protection_enabled = false
 
   attributes = [
